b9fc034cbf7ea6d75de86abe09808f623f72df34
akyds <ww.ayazkhan.com.21@gmail.com>
2026-05-29 08:08:39 -0400

    States have been added

---
+++ src/components/states.py
@@ -0,0 +1,15 @@
+from typing import List, Annotated, TypedDict
+from langchain.messages import AnyMessage
+import operator
+
+
+class TravelState(TypedDict):
+    messages:Annotated[List[AnyMessage],operator.add]
+    user_query:str
+    suggested_cities:List
+    selected_city:str
+    bus_routes:str
+    hotel_results:str
+    estimated_budget:str
+    iterniary:str
+    llm_calls:int
